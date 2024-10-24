d39a816c77f7020890d8e1afc844c2913b16c71a
Sebastián Candia Cabezas <s.candia05@ufromail.cl>
2024-10-24 00:04:50 -0300

    Modificaciones

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://200.13.4.230:3000",
     methods: ["GET", "POST"]
   }
 });
--- VariaMosPLE/src/Utils/Socket.tsx
+++ VariaMosPLE/src/Utils/Socket.tsx
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 
 // Solo necesitas configurar la conexión una vez
-const socket = io('http://localhost:4000');
+const socket = io('http://200.13.4.230:4000');
 
 // Exportar la misma instancia para ser usada en cualquier parte de la aplicación
 export default socket;
