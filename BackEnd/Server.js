0f4ad70820c43e3dfa4068ed07b89b0791b5699f
Seba2424 <s.calfunao01@ufromail.cl>
2024-10-24 05:05:25 +0000

    Paso a produccion

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://ceis.variamos.com:3000",
+    origin: "https://ceis.variamos.com",
     methods: ["GET", "POST"]
   }
 });
